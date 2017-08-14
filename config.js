d9a3afdd5dcf571b231a6584db7f63f8599e4531
lvdesign <mailbox@lvdesign.com.fr>
2017-08-14 11:09:18 +0200

    New config
    Restaure config

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    //'mongoUrl' : 'mongodb://lvrecipes:LVrecipes2017@ds153392.mlab.com:53392/recipeslv'
+    /*
+    * local mongodb
+    */
     //'mongoUrl' : 'mongodb://localhost:27017/recipeslv'
-   'mongoUrl' :'mongodb://laurentV:2000LVrecipe@ds153392.mlab.com:53392/recipeslv'
-
+    
+    /*
+    * Link to mongoDB to mlab.com
+    */
+   //mongodb://<dbuser>:<dbpassword>@dsXXXXX.mlab.com:XXXXX/recipeslv
 }
-
-//lvrecipes
-//LVrecipes2017
-//mongodb://<dbuser>:<dbpassword>@ds153392.mlab.com:53392/recipeslv
-//mongodb://lvrecipes:LVrecipes2017@ds153392.mlab.com:53392/recipeslv
